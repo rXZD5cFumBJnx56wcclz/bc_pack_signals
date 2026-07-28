1f16322f6348f9b6dd1ceba8eaa6f62bdf602ed8
Your Name <163586801+rXZD5cFumBJnx56wcclz@users.noreply.github.com>
2026-07-29 01:20:40 +0300

    major

--- src/lib.rs
+++ src/lib.rs
@@ -2,136 +2,126 @@
 
 use std::sync::LazyLock;
 
-use bc_signals::ready::prelude::*;
-use bc_signals::ready::{
+use bc_signals::prelude::*;
+use bc_signals::{
     change_signal::CHANGE_SIGNAL, change_src::CHANGE_SRC, convert::CONVERT, copy::COPY,
     filter::FILTER, invert::INVERT, th::TH, repeat::REPEAT,
     set_probability::SET_PROBABILITY,
 };
-use bc_utils_lg::types::maps::MAP;
+use bc_utils_lg::types::maps::{PACK};
 
 use bc_utils_lg::structs::settings::SETTINGS_SIGNAL;
 
-pub static FUNCS_EXTRACT_ARGS: LazyLock<fn() -> MAP<&'static str, fn(&SETTINGS_SIGNAL) -> Box<dyn SignalReady>>> = LazyLock::new(|| {
-    || {
+pub type PACK_TYPE_SIGN = PACK<SETTINGS_SIGNAL, Box<dyn SignalReady>>;
+
+pub static PACK: LazyLock<PACK_TYPE_SIGN> = LazyLock::new(|| {
         MAP::from_iter([
             (
                 "th",
                 (|setting: &SETTINGS_SIGNAL| {
                     let mut df = TH::default();
-                    df.set_th_min(*setting.kwargs_f64.get("th_min").unwrap_or(&df.th_min));
-                    df.set_th_max(*setting.kwargs_f64.get("th_max").unwrap_or(&df.th_max));
-                    df.set_limit(*setting.kwargs_f64.get("limit").unwrap_or(&df.limit));
-                    df.set_index_min(
-                        *setting
+                    df.params.th_min = setting.kwargs_f64.get("th_min").copied().unwrap_or(df.params.th_min);
+                    df.params.th_max = setting.kwargs_f64.get("th_max").copied().unwrap_or(df.params.th_max);
+                    df.params.limit = setting.kwargs_f64.get("limit").copied().unwrap_or(df.params.limit);
+                    df.params.index_min = 
+                        setting
                             .kwargs_usize
                             .get("index_min")
-                            .unwrap_or(&df.index_min),
-                    );
-                    df.set_index_max(
-                        *setting
+                            .copied()
+                            .unwrap_or(df.params.index_min);
+                    df.params.index_max = 
+                        setting
                             .kwargs_usize
                             .get("index_max")
-                            .unwrap_or(&df.index_max),
-                    );
-                    df.set_index_normal(
-                        *setting
+                            .copied().unwrap_or(df.params.index_max);
+                    df.params.index_normal = 
+                        setting
                             .kwargs_usize
                             .get("index_normal")
-                            .unwrap_or(&df.index_normal),
-                    );
+                            .copied().unwrap_or(df.params.index_normal);
                     Box::new(df) as Box<dyn SignalReady>
                 }) as fn(&SETTINGS_SIGNAL) -> Box<dyn SignalReady>,
             ),
             (
                 "set_probability",
-                (|_: &SETTINGS_SIGNAL| Box::new(SET_PROBABILITY::new()) as Box<dyn SignalReady>),
+                (|_: &SETTINGS_SIGNAL| Box::new(SET_PROBABILITY) as Box<dyn SignalReady>),
             ),
             (
                 "change_signal",
-                (|_: &SETTINGS_SIGNAL| Box::new(CHANGE_SIGNAL::new()) as Box<dyn SignalReady>),
+                (|_: &SETTINGS_SIGNAL| Box::new(CHANGE_SIGNAL::default()) as Box<dyn SignalReady>),
             ),
             (
                 "change_src",
                 (|setting: &SETTINGS_SIGNAL| {
                     let mut df = CHANGE_SRC::default();
-                    df.set_signal_short(
-                        *setting
+                    df.params.signal_short =
+                        setting
                             .kwargs_f64
                             .get("signal_short")
-                            .unwrap_or(&df.signal_short),
-                    );
-                    df.set_signal_long(
-                        *setting
+                            .copied().unwrap_or(df.params.signal_short);
+                    df.params.signal_long = 
+                        setting
                             .kwargs_f64
                             .get("signal_long")
-                            .unwrap_or(&df.signal_long),
-                    );
-                    df.set_signal_hold(
-                        *setting
+                            .copied().unwrap_or(df.params.signal_long);
+                    df.params.signal_hold = 
+                        setting
                             .kwargs_f64
                             .get("signal_hold")
-                            .unwrap_or(&df.signal_hold),
-                    );
+                            .copied().unwrap_or(df.params.signal_hold);
                     Box::new(df) as Box<dyn SignalReady>
                 }),
             ),
             (
                 "convert",
-                (|_: &SETTINGS_SIGNAL| Box::new(CONVERT::new()) as Box<dyn SignalReady>),
+                (|_: &SETTINGS_SIGNAL| Box::new(CONVERT) as Box<dyn SignalReady>),
             ),
             (
                 "invert",
                 (|setting: &SETTINGS_SIGNAL| {
                     let mut df = INVERT::default();
-                    df.set_signal_short(
-                        *setting
+                    df.signal_short = 
+                        setting
                             .kwargs_f64
                             .get("signal_short")
-                            .unwrap_or(&df.signal_short),
-                    );
-                    df.set_signal_long(
-                        *setting
+                            .copied().unwrap_or(df.signal_short);
+                    df.signal_long = 
+                        setting
                             .kwargs_f64
                             .get("signal_long")
-                            .unwrap_or(&df.signal_long),
-                    );
-                    df.set_signal_hold(
-                        *setting
+                            .copied().unwrap_or(df.signal_long);
+                    df.signal_hold = 
+                        setting
                             .kwargs_f64
                             .get("signal_hold")
-                            .unwrap_or(&df.signal_hold),
-                    );
+                            .copied().unwrap_or(df.signal_hold);
                     Box::new(df) as Box<dyn SignalReady>
                 }),
             ),
             (
                 "filter",
-                (|_: &SETTINGS_SIGNAL| Box::new(FILTER::new()) as Box<dyn SignalReady>),
+                (|_: &SETTINGS_SIGNAL| Box::new(FILTER) as Box<dyn SignalReady>),
             ),
             (
                 "copy",
-                (|_: &SETTINGS_SIGNAL| Box::new(COPY::new()) as Box<dyn SignalReady>),
+                (|_: &SETTINGS_SIGNAL| Box::new(COPY) as Box<dyn SignalReady>),
             ),
             (
                 "repeat",
                 (|setting: &SETTINGS_SIGNAL| {
                     let mut df = REPEAT::default();
-                    df.set_value_signal(
-                        *setting
+                    df.value_signal = 
+                        setting
                             .kwargs_f64
                             .get("value_signal")
-                            .unwrap_or(&df.value_signal),
-                    );
-                    df.set_value_probability(
-                        *setting
+                            .copied().unwrap_or(df.value_signal);
+                    df.value_probability = 
+                        setting
                             .kwargs_f64
                             .get("value_probability")
-                            .unwrap_or(&df.value_probability),
-                    );
+                            .copied().unwrap_or(df.value_probability);
                     Box::new(df) as Box<dyn SignalReady>
                 }),
             ),
         ])
-    }
 });
